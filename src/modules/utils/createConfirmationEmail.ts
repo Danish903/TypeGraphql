095e5d8e543410b72e26670a29cad9eee0554952
Anish Dhungel <adhungel@ucdavis.edu>
2019-04-08 20:58:06 -0700

    changed userid type number to string

--- src/modules/utils/createConfirmationEmail.ts
+++ src/modules/utils/createConfirmationEmail.ts
@@ -2,7 +2,7 @@ import { v4 } from "uuid";
 import { redis } from "../../redis";
 import { emailConfirmationPrefix } from "../constants/redisPrefixes";
 
-export const createConfimationEmail = async (userId: number) => {
+export const createConfimationEmail = async (userId: string) => {
    const token = v4();
 
    await redis.set(emailConfirmationPrefix + token, userId, "ex", 60 * 60 * 24); // 1 day expiration
